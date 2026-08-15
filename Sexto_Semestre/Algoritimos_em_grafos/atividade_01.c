6206338cec20db1088230b3167231b4dfbb4cd8d
koreano06 <gusttavo06gomes06@gmail.com>
2026-08-14 21:16:38 -0300

    correção da atividade 01

--- Sexto_Semestre/Algoritimos_em_grafos/atividade_01.c
+++ Sexto_Semestre/Algoritimos_em_grafos/atividade_01.c
@@ -4,7 +4,7 @@ int main() {
     int matriz[5][5], mult[5][5];
     int i, j, contador = 1;
 
-    printf("--- Matriz Original ---\n");
+    printf("Matriz principal:\n");
     for (i = 0; i < 5; i++) {
         for (j = 0; j < 5; j++) {
             matriz[i][j] = contador++;
@@ -13,7 +13,7 @@ int main() {
         printf("\n");
     }
 
-    printf("\n--- Matriz ao Quadrado ---\n");
+    printf("\n Matriz ao Quadrado:\n");
     for (i = 0; i < 5; i++) {
         for (j = 0; j < 5; j++) {
             mult[i][j] = matriz[i][j] * matriz[i][j];
